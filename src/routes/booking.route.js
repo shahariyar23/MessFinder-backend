216643d75625aa32a7039b5bab40cfc7dbdb3bdc
shahariyar23 <shahariyar2307@gmail.com>
2025-10-15 01:57:22 +0600

    some route path name change for booking

--- src/routes/booking.route.js
+++ src/routes/booking.route.js
@@ -16,8 +16,8 @@ bookingRoute.route("/create-booking").post(authMiddleware, createBooking);
 bookingRoute.route("/get-user-booking").get(authMiddleware, getUserBookings);
 bookingRoute.route("/get-owner-booking").get(authMiddleware, getOwnerBookings);
 bookingRoute.route("/get-booking-info/:bookingId").get(authMiddleware, getBookingById);
-bookingRoute.route("/get-booking-status/:bookingId").post(authMiddleware, updateBookingStatus);
-bookingRoute.route("/get-booking-payment-status/:bookingId").post(authMiddleware, updatePaymentStatus);
+bookingRoute.route("/update-booking-status/:bookingId").post(authMiddleware, updateBookingStatus);
+bookingRoute.route("/update-booking-payment-status/:bookingId").post(authMiddleware, updatePaymentStatus);
 bookingRoute.route("/cancel-booking/:bookingId").get(authMiddleware, cancelBooking);
 bookingRoute.route("/delete-booking/:bookingId").get(authMiddleware, deleteBooking);
 
