6b46e8a61eeef2d01216697c3cedbe10f6530dbd
shahariyar23 <shahariyar2307@gmail.com>
2025-10-22 02:58:20 +0600

    feat: Update email transporter configuration for improved reliability

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -5,11 +5,16 @@ dotenv.config();
 
 // Create transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+   host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
   },
+  connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000
 });
 
 // Verify transporter
